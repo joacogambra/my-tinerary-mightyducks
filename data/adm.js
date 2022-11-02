98de957e70d5bcc884e26e94b0d50ad3df2153fa
LailaJalil <lailajalil@gmail.com>
2022-11-02 20:34:00 -0300

    arrays de objetos de hotels,adm y shows

---
+++ data/adm.js
@@ -0,0 +1,50 @@
+let adm=[{
+    id: "admin1",
+    name: "Eric",
+    lastName: "Rodriguez",
+    age: 23,
+    email: "feric.rodriguez@gmail.com",
+    password: "Chau6789",
+    code: "ryovxw2v",
+    verified: true, 
+    logged: true,
+
+},
+{
+    id: "admin2",
+    name: "Laila",
+    lastName: "Jalil",
+    age: 36,
+    email: "lailajalil@gmail.com",
+    password: "Aprobado2",
+    code: "aj7h57ow",
+    verified: true, 
+    logged: true,
+
+},
+{ 
+    id: "admin3",
+    name: "Joaquin",
+    lastName: "Gambra",
+    age: 24,
+    email: "joaquingambra@gmail.com",
+    password: "Aprobado1",
+    code: "agkuu4yy",
+    verified: true, 
+    logged: true,
+},
+{
+    id: "admin4",
+    name: "Juan",
+    lastName: "Perez",
+    age: 40,
+    email: "feric.rodriguez@gmail.com",
+    password: "Chau1234",
+    code: "w8b6y3m2",
+    verified: true, 
+    logged: true,
+
+}
+
+]
+export default adm
---
+++ data/shows.js
@@ -0,0 +1,66 @@
+let shows = [
+    {id: it1,
+        hotelId: hbarce,
+        name: "Jackies",
+        description: "Jackies says goodbye to his weekly residency on Wednesdays at W Barcelona. This is goodbye and see you soon at the same time. Do not miss this unique edition at the elegant Eclipse club on the 26th floor of 26th W Barcelona, with the presence of one of the most in-form artists and producers on the national scene with a special House Music set that will leave no one different. Something unique as always on our favorite Wednesdays.",
+        photo: ("https://imgproxy.ra.co/_/quality:66/w:1500/rt:fill/aHR0cHM6Ly9pbWFnZXMucmEuY28vNmJlNjE2OGZkMDZkYjY1OTA0ODQ0M2Q4Yjc4NjMxMjViNzJjYWFjNC5qcGc="),
+        price: 20,
+        date: 2022-12-12,
+        userId:  user1 ,
+        
+    },
+    {id: it2,
+        hotelId: hmilan,
+        name: "The Milan Coffee Festival",
+        description: "The Festival will feature live experiences from coffee, food and equipment suppliers. Visitors will enjoy endless free coffee tastings, interactive workshops, demonstrations from world-class baristas, delicious street food, coffee cocktails and so much more.",
+        photo: ("https://www.milancoffeefestival.com/getattachment/Venue-FAQ/Exhibitors-2.jpg.aspx?width=750&height=500"),
+        price: 32,
+        date: 2022-11-12,
+        userId:  user1 ,
+        
+    },
+    {id: it3,
+        hotelId: hoamsterdam,
+        name: "Charlie Puth",
+        description: "The singer, songwriter and producer will be in the Netherlands exclusively in Carré with a seated concert, which will be dedicated to the album CHARLIE, which will be released on 7 October.",
+        photo: ("https://assets.carre.nl/fd880002-0dcd-4ee5-8f88-9f25c38db680/-/crop/9000x6000/0,0/-/format/auto/-/quality/lightest/-/progressive/yes/-/max_icc_size/10/-/resize/1920x/"),
+        price: 174,
+        date: 2022-12-4,
+        userId:  user1 ,
+        
+    },
+    {id: it4,
+        hotelId: hprague,
+        name: "Mozart Dinner",
+        description: "Two opera singers from Czech Opera Houses, instrumentalists from the Prague Radio Symphony Orchestra and excellent Salzburg Chamber Soloists form a musical ensemble Amadeus Prague. They are dressed in historical costumes during the performance and present the most famous arias and duets from the operas “Don Giovanni” (world premiere in Prague 1787), “The Marriage of Figaro” and “The Magic Flute“. Enjoy opera at your hand in the heart of Prague!",
+        photo: ("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuDqEErZc-CYG0lV14rktO1WnskL8JiEgN4Q&usqp=CAU"),
+        price: 100,
+        date: 2022-12-3,
+        userId:  user1 ,
+        
+    },
+    {id: it5,
+        hotelId: hny,
+        name: "IGC - @ONLYVADA",
+        description: "Take music to new heights from the 34th floor of RT60. Our showstopping bar & lounge in the heart of Times Square features two outdoor terraces that offer north and south facing city views. Pair cocktails and appetizers with unexpected mixes from new to well-known DJs. Music-infused design resonates throughout including music memorabilia from world-renowned artists who were influenced by the glamor and grit of New York City",
+        photo: ("https://www.hardrockhotels.com/new-york/files/6382/unplash_dj_neon.jpg"),
+        price: 100,
+        date: 2022-11-11,
+        userId:  user1 ,
+        
+    },
+    {id: it6,
+        hotelId: histanbul,
+        name: "Istanbul International Film Festival",
+        description: "Istanbul’s International Film Festival is 12 days long film festival, organized by Istanbul Foundation for Culture and Arts (IKSV). At this city’s leading film festival in Istanbul in April, you can discover and enjoy remarkable local and international films in competition, at the several culture centers and cinemas in the Beyoglu and Kadikoy districts. Other highlights include, various activities and workshops throughout the city.",
+        photo: ("https://istanbeautiful.com/wp-content/uploads/istanbul-film-festival.jpg"),
+        price: 50,
+        date: 2023-4-8,
+        userId:  user1 ,
+        
+    }
+    
+    ]
+
+
+    export default shows
