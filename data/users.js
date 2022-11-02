b599a728f1633f956c3545159b7a85c3440a886e
joacogambra <joaquingambra@gmail.com>
2022-11-02 17:56:40 -0300

    users

---
+++ data/users.js
@@ -0,0 +1,47 @@
+let users = [
+    {
+        id: 'user1',
+        name: 'Ignacio',
+        lastName: 'Borraz',
+        age: 32,
+        email: 'ignacioborraz@hotmail.com',
+        password: 'Hola1234',
+        code: 32,//'cualquier-codigo-aleatorio'
+        verified: true,
+        logged: true,
+    },
+    {
+        id: 'user2',
+        name: 'Lionel',
+        lastName: 'Messi',
+        age: 35,
+        email: 'leomessi10@hotmail.com',
+        password: 'thiagociromateo',
+        code: 10,
+        verified: true,
+        logged: true,
+    },
+    {
+        id: 'user3',
+        name: 'Anne',
+        lastName: 'Hathaway',
+        age: 39,
+        email: 'annehathaway@gmail.com',
+        password: 'diariodeunaprincesa',
+        code: 2000,
+        verified: true,
+        logged: true,
+    },
+    {
+        id: 'user4',
+        name: 'Julian',
+        lastName: 'Alvarez',
+        age: 22,
+        email: 'juli9@gmail.com',
+        password: 'riverplate33',
+        code: 9,
+        verified: true,
+        logged: true,
+    }
+]
+export default users
