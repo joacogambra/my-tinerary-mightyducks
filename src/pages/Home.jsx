43c7b3303292fbd607ee01d50bc3ff86cdb97af2
joacogambra <joaquingambra@gmail.com>
2022-11-03 18:24:21 -0300

    homejsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import HeaderVideo from '../components/HeaderVideo'
+import Footer from '../components/Footer'
 
 export default function home() {
   return (
+    <>
     <HeaderVideo />
+    <Footer />
+    </>
   )
 }
