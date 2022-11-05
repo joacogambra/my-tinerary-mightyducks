76280ece522559a9227e97a0dc5885b8078ad4b4
LailaJalil <lailajalil@gmail.com>
2022-11-05 00:16:19 -0300

    carousel funcionando con intervalo y manualmente

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,20 @@ import HeaderVideo from '../components/HeaderVideo'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Logo from '../components/Logo'
-
+import Home1 from '../layouts/Home1'
+import Carousel from '../components/Carousel'
 export default function home() {
   return (
-    <>
     
-    <HeaderVideo componenttop=<Header/> componentmiddle=<Logo/> componentfotter=  <Footer />/>
-    
-    </>
+    <Home1>
+        
+         <HeaderVideo componenttop=<Header></Header> componentmiddle=<Logo/>/>
+         <div className='backcolor'>
+         <Carousel></Carousel>
+         </div>
+         <Footer></Footer>
+       
+        </Home1>
+   
   )
 }
