e915900913f93a0fb1d8400581a156e8f63a7f66
joacogambra <joaquingambra@gmail.com>
2022-11-06 19:40:15 -0300

    Cities

---
+++ src/pages/Cities.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import HeaderVideo from '../components/HeaderVideo'
+import Header from '../components/Header'
+import CardCities from '../components/CardCities'
+
+
+
+export default function Cities() {
+  return (
+    <>
+    <HeaderVideo componenttop={<Header/>} componentmiddle={<CardCities/>} />
+      <Footer/>     
+    </>
+  )
+}
