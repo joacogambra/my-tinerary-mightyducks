a717f702f22027560572f2ad2058fa23e7cd1dca
joacogambra <joaquingambra@gmail.com>
2022-11-07 03:10:32 -0300

    autototop

--- src/components/AutoToTop.jsx
+++ src/components/AutoToTop.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'  
 
 export default function AutoToTop() {
-    let {top} = useLocation()    
-        useEffect(() => { window.scrollTo(0, 0); }, [top])     
+    let {pathname} = useLocation()    
+        useEffect(() => { window.scrollTo(0, 0) }, [pathname])     
             return null
 }
