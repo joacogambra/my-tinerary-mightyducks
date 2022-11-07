63444fcf77c693329ded28ee8356962d19f24614
joacogambra <joaquingambra@gmail.com>
2022-11-07 03:14:18 -0300

    Appjs

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Home from "./pages/Home"
 import Cities from "./pages/Cities"
 import Hotels from "./pages/Hotels"
 import SignIn from "./pages/SignIn"
-import City from "./pages/City"
+import CityPage from "./pages/CityPage"
 import Hotel from "./pages/Hotel"
 import AutoToTop from "./components/AutoToTop"
 import {Routes, Route} from 'react-router-dom'
 import NotFound from "./pages/NotFound"
 import SignUpPage from "./pages/SignUpPage"
+import NewCity from "./pages/NewCity"
 
 function App() {
   return (
@@ -19,12 +20,12 @@ function App() {
       <Route path="/home" element={<Home/>}/>
       <Route path="/hotels" element={<Hotels/>}/>
       <Route path="/cities" element={<Cities/>}/>
+      <Route path="/newcity" element={<NewCity/>}/>
       <Route path="/sign-up" element={<SignUpPage/>}/>
       <Route path="/sign-in" element={<SignIn/>}/>
-      <Route path="/city" element={<City/>}/>
-      <Route path="/hotel" element={<Hotel/>}/>
+      <Route path="/:id" element={<CityPage/>} />
+      <Route path="/:id" element={<Hotel/>} />
       <Route path="/*" element={<NotFound/>}/>
-  
     </Routes>
     </main>
     
