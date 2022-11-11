b6b44e396c3abf4e507a96f55abce418131200eb
LailaJalil <lailajalil@gmail.com>
2022-11-11 15:43:25 -0300

    correción de error en codigo de axios y ruta con api/hotels

--- src/components/FormHotel.jsx
+++ src/components/FormHotel.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useState, useEffect} from 'react'
+import {useState } from 'react'
 import axios from 'axios'
 import { BASE_URL } from '../Api/url';
 
@@ -8,41 +8,39 @@ export default function FormHotel() {
 
   let [form, setForm] = useState({});
 
-  console.log(form);
-  useEffect(()=>{
-    axios.post(`${BASE_URL}/hotels/`, form)
-    .then(response=>
-      console.log(response))
-    .catch(error=> console.log(error))
-
-  },[form])
-
 
 let handleChange = (e) => {
+
   setForm({
     ...form,
    [ e.target.name]: e.target.value,
   })
+  console.log(form);
 }   
   
 let handleSubmit=(e)=>{
   e.preventDefault()
+  console.log(e);
+  axios.post(`${BASE_URL}/api/hotels/`, form)
+    .then(response=>
+      console.log(response))
+    .catch(error=> console.log(error))
  
 }
 
 
       return (
         <>
-     <form className='sign-in' onSubmit={handleSubmit} >
+     <form className='sign-in'   >
          
          <h3> Enter the Hotel information</h3>
-       <input name= "name"  type="text"  placeholder='Hotel´s Name' onChange={handleChange}/>
-       <input name="photo" type="text"  placeholder='Photo' onChange={handleChange}/>
-       <input name="capacity" type="number"  placeholder='Capacity' onChange={handleChange}/>
-       <input name="cityId" type="text"  placeholder='City Id' onChange={handleChange}/>
-       <input name="userId" type="text"  placeholder='Your Id' onChange={handleChange}/>
+       <input name= "name"  type="text"  placeholder='Hotel´s Name' onChange={handleChange} required/>
+       <input name="photo" type="text"  placeholder='Photo' onChange={handleChange} required/>
+       <input name="capacity" type="number"  placeholder='Capacity' onChange={handleChange} required/>
+       <input name="cityId" type="text"  placeholder='City Id' onChange={handleChange} required/>
+       <input name="userId" type="text"  placeholder='Your Id' onChange={handleChange} required/>
 
-       <button  className=' button login'>Add Hotel</button>
+       <button  className=' button login' onClick={handleSubmit} >Add Hotel</button>
        </form>
        
 
