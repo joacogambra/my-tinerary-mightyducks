1ec5ae880cc7e1f93532119311293a47726bb100
LailaJalil <lailajalil@gmail.com>
2022-11-14 16:08:21 -0300

    corrección de carousel

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Inputs(props) {
     let {dato,type,placeholder,listener}= props
   return (
-    <React.StrictMode>
+    
             <label>
                 <input type={type} name={dato} placeholder={placeholder} onChange={listener}  required ></input>
             </label>
-            </React.StrictMode>
+
   )
 }
--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -4,7 +4,7 @@ export default function Select(props) {
 
     let {value1, value2, onchange}= props
   return (
-    <select defaultValue={'DEFAULT'} onChange={(e)=>{(onchange(e.target.value))}}>
+    <select defaultValue={'DEFAULT'} onChange={onchange}>
         <option value="DEFAULT" disabled>SortBy:</option>
         <option value={value1}>Ascending</option>
         <option value={value2}>Descending</option>
