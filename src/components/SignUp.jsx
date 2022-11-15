a2d04bf80e96b91aaaba24a3a285b3a5569f615a
joacogambra <joaquingambra@gmail.com>
2022-11-14 22:03:12 -0300

    componentSignUp

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -25,25 +25,13 @@ export default function SignUp() {
 
   return (
       <form action="#" onSubmit= {registration} ref={signup} className='sign-in'  >
-      <h3 class="title">Registration</h3>
-        <div> 
-            <input ref={fullName} type="text" placeholder="Enter your FullName" required/>
-         </div>
-          <div>
-            <input ref={userName} type="text" placeholder="Enter your Username" required/>
-          </div>
-          <div>
-            <input ref={email} type="text" placeholder="Enter your email" required/>
-          </div>
-          <div>
-            <input ref={phone} type="text" placeholder="Enter your Phone Number" required/>
-          </div>
-          <div>
-            <input ref={pass} type="password" placeholder="Create your password" required/>
-          </div>
-          <div >
-            <input type="password" placeholder="Confirm your password" required/>
-          </div>
+      <h3 class="title">Registration</h3>        
+            <input ref={fullName} type="text" placeholder="Enter your FullName" required/>                  
+            <input ref={userName} type="text" placeholder="Enter your Username" required/>                 
+            <input ref={email} type="text" placeholder="Enter your email" required/>                 
+            <input ref={phone} type="text" placeholder="Enter your Phone Number" required/>                   
+            <input ref={pass} type="password" placeholder="Create your password" required/>                 
+            <input type="password" placeholder="Confirm your password" required/>        
         <div class="button login">
           <input type="submit" value="Register"/>
         </div>
