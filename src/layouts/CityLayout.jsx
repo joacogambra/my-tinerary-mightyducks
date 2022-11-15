c895d78883390d5e89a8a2402e0749575db70040
joacogambra <joaquingambra@gmail.com>
2022-11-14 22:04:28 -0300

    layoutCity

--- src/layouts/CityLayout.jsx
+++ src/layouts/CityLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Activities from '../components/Activities'
-import City from '../components/City'
+// import City from '../components/City'
 import Header from '../components/Header'
 import HeaderVideo from '../components/HeaderVideo'
 import Footer from '../components/Footer'
@@ -9,9 +9,8 @@ import Footer from '../components/Footer'
 export default function CityLayout() {
   return (
       <>
-      <HeaderVideo componenttop=<Header/> componentmiddle= <City/> />
+      <HeaderVideo componenttop={<Header/>} componentmiddle={<Activities/>} />
       <div className='flex-row grow gap evenly'>
-      <Activities/>
       </div>
       <Footer/>
    
