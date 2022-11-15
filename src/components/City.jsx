f36c4c4630a77befdd102eccbb38c01be585ad3c
joacogambra <joaquingambra@gmail.com>
2022-11-14 22:02:09 -0300

    componentCity

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,23 +1,29 @@
 import React from 'react'
-import cities from '../data/cities'
 import { useParams } from 'react-router-dom'
 import Details from './Details'
 import NotFound from './NotFound'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { BASE_URL } from '../Api/url'
 
 export default function City() {
    const { id }= useParams()
+   let[currentCity, setCurrentCity] = useState([])
 
-    let currentCity= cities.find(elemento=> elemento.id=== id)
-    if (currentCity === undefined) {
-      return <NotFound/>
-     } else {
-      
-
-  return (
-    <div>
-        <Details name={currentCity.name} image={currentCity.photo} number={currentCity.population} category="Population"></Details>
-    </div>
-    
+    useEffect(() =>{ 
+      axios.get(`${BASE_URL}/cities/${id}`)
+       .then(res=> setCurrentCity(res.data.response))
+       .catch(error=>error.message)
+       // eslint-disable-next-line react-hooks/exhaustive-deps
+       }, [])
+ 
+    if (currentCity.length === 0) {
+        return <NotFound/>
+    } else {
+    return (
+      <div>
+          <Details name={currentCity.name} image={currentCity.photo} number={currentCity.population} category="Population"></Details>
+      </div>  
   )
 }
 }
