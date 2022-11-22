ff01828355e38752ae2567e5790e060b52fbf27c
joacogambra <joaquingambra@gmail.com>
2022-11-21 22:13:38 -0300

    reducer cities

--- src/redux/reducers/citiesReducer.js
+++ src/redux/reducers/citiesReducer.js
@@ -1,11 +1,15 @@
 import { createReducer } from "@reduxjs/toolkit";
 import citiesAction from "../actions/citiesAction";
 import newCityAction from "../actions/newCityAction";
+import myCitiesAction from "../actions/myCitiesAction";
+
 
 const {newCity} = newCityAction
 const {getFiltering} = citiesAction
+const {deleteOneCity} = myCitiesAction
 const initialState = {
     cities:[],
+    myCities: []
 }
 
 const citiesReducer = createReducer(initialState, (builder)=>{
@@ -26,6 +30,12 @@ const citiesReducer = createReducer(initialState, (builder)=>{
             if(action.payload.success) {
                 state.cities.push(action.payload.success)
             }      
+    })
+    .addCase(deleteOneCity.fulfilled, (state,action)=>{
+        return {
+            ...state,
+            myCities: state.myCities.filter((city) => city._id !== action.payload.cityDeleted),
+          }
     })
 })
 
