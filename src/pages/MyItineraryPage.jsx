b363a81c82d609345fd59d4b864b24879a1f1fa2
joacogambra <joaquingambra@gmail.com>
2022-11-28 00:55:01 -0300

    page my itinerary

--- src/pages/MyItineraryPage.jsx
+++ src/pages/MyItineraryPage.jsx
@@ -3,12 +3,13 @@ import Footer from '../components/Footer'
 import HeaderVideo from '../components/HeaderVideo'
 import Header from '../components/Header'
 import MyItinerary from '../components/MyItinerary'
+import FormNewItinerary from '../components/FormNewItinerary'
 
 export default function MyItineraryPage() {
   return (
     <>
     <HeaderVideo componenttop={<Header/>} componentmiddle={<MyItinerary/>}/>
-    
+    <FormNewItinerary/>
     
       <Footer/>     
     </>
