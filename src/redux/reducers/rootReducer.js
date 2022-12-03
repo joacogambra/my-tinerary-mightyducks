ce8e18fadf1237f3fa71ad292018a31145b8c6e1
joacogambra <joaquingambra@gmail.com>
2022-12-03 01:50:36 -0300

    root reducer

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -5,8 +5,9 @@ import itineraryReducer from './itineraryReducer'
 import showReducer from './showReducer'
 import carouselReducer from './carouselReducer';
 import userReducer from './userReducer';
+import reactionReducer from './reactionReducer';
 
 const rootReducer ={
-    hotelsReducer, citiesReducer, citiesFilterReducer, itineraryReducer, showReducer, carouselReducer,userReducer,
+    hotelsReducer, citiesReducer, citiesFilterReducer, itineraryReducer, showReducer, carouselReducer,userReducer,reactionReducer
 }
 export default rootReducer
