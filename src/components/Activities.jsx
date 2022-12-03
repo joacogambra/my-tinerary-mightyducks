8b22eeee3f95eaa39fbad38a5424ad7ea89d5079
LailaJalil <lailajalil@gmail.com>
2022-12-03 11:51:40 -0300

    arreglo comentarios

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -4,8 +4,8 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { BASE_URL } from '../Api/url'
 import CardActivities from './CardActivities'
-import ShowCard from './ShowCard'
-import Comments from './Comments'
+// import ShowCard from './ShowCard'
+// import Comments from './Comments'
 
 export default function Detail(props) {
    const { id }= useParams()
@@ -19,18 +19,30 @@ export default function Detail(props) {
     }, [])
   
   return ( 
-        activity.map((activity)=>(
-           <ShowCard name={activity.name} photo={activity.photo[0]} id={activity._id} description={activity.description} value='itineraryId'>
-           <>
-             <div className='insta-price' >
-             <p>Price: ${activity.price}</p>
-             <p>Duration: {activity.duration}hs</p>
-             </div>
-            <div>
-             { <Comments id={activity._id} value='itineraryId'/>   }
-            </div>
-            </>
-         </ShowCard>
+    <div className='flex-row gap'>
+
+   {     activities.map((activity)=>(
+              <CardActivities name={activity.name}  image={activity.photo[0]} image1={activity.photo[1]} id={activity._id} continente={activity.description} price={activity.price} itinerary={activity} key={activity.name}  value='itineraryId'>
+        
+
+              </CardActivities>
+
+
+
+        ))}
+        </div>
+      //   activities.map((activity)=>(
+      //      <ShowCard name={activity.name} photo={activity.photo[0]} id={activity._id} description={activity.description} value='itineraryId'>
+      //      <>
+      //        <div className='insta-price' >
+      //        <p>Price: ${activity.price}</p>
+      //        <p>Duration: {activity.duration}hs</p>
+      //        </div>
+      //       <div>
+      //        { <Comments id={activity._id} value='itineraryId'/>   }
+      //       </div>
+      //       </>
+      //    </ShowCard>
   
-       ))
+      //  ))
 )}
