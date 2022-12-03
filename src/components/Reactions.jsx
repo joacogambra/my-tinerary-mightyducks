57844fe4738e125b9079fa75f6fd7d2db6034923
joacogambra <joaquingambra@gmail.com>
2022-12-03 01:48:15 -0300

    component reactions

---
+++ src/components/Reactions.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { useState } from "react"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import actionForReaction from "../redux/actions/actionForReaction"
+import ReactionsLike from "./ReactionsLike"
+
+export default function Reaction(props) {
+  
+
+
+  return (
+    <div className="uniqueReaction">
+      {/* {uniqueReaction.map((i) => (<ReactionsLike reaction={i} key={i._id} itineraryId={itinerary}/>))} */}
+    </div>
+  )
+}
+
+
